8e7c2fb1ba7a31e07e52e8c402291544d1f47fca
Javier2DAWLaMerce <jorduna.daw2n22@lamerce.com>
2023-02-23 16:07:46 +0100

    subo cosas y haciendo formulario login

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Dotenv\Exception\ValidationException;
+use Illuminate\Http\Request;
+
+class SessionsController extends Controller
+{
+    public function destroy()
+    {
+        auth()->logout();
+        return redirect('/login')->with('logoutRealizado', '¡Hasta la próxima!');
+    }
+
+    public function create()
+    {
+        return view('sessions.create');
+    }
+    public function store()
+    {
+        //validamos la peticion
+        $atributs = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        //Intentemos autenticar el usuario, fi falla lanzamos una excepcion
+        if (!auth()->attempt($atributs)) {
+            //El mensaje aparece debajo del campo que le indicamos
+            throw ValidationException::withMessages([
+                'email' => 'Sus credenciales no han podido ser verificadas.',
+            ]);
+        }
+        //SE aconseja regenerar la session para evitar ataques de 'Session fixation'
+        //https://owasp.org/www-community/attacks/Session_fixation
+        session()->regenerate();
+        
+        //Redirigimos con un mesnaje flash
+        return redirect('/')->with('inicioSesion','¡Bienvenido!');
+    }
+}
